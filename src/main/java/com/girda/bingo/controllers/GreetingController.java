b59321f63bea0a06202fcb137859f86ee197d5d5
girdacio <girdacio@gmail.com>
2021-07-18 22:53:30 -0300

    versao teste websocket funcionando com STOMP e SOCKJS

---
+++ src/main/java/com/girda/bingo/controllers/GreetingController.java
@@ -0,0 +1,21 @@
+package com.girda.bingo.controllers;
+
+import com.girda.bingo.models.Greeting;
+import com.girda.bingo.models.HelloMessage;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class GreetingController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {        
+        System.out.println("chegou! = " + message.getName());
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+
+}
